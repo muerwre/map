363e6cd0610d2c5365df3faca583ff32612ada86
Fedor Katurov <fkaturov@allmax.team>
2018-08-17 11:24:53 +0700

    4

--- engine/composerOrchid.php
+++ engine/composerOrchid.php
@@ -36,13 +36,13 @@ $path = isset($_REQUEST['path']) ? $_REQUEST['path'] : null;
 $points = isset($_REQUEST['points']) ? $_REQUEST['points'] : null;
 $stickers = isset($_REQUEST['stickers']) ? $_REQUEST['stickers'] : null;
 
-//print_r($path);exit;
 $im = new Imagick();
 $im->newImage($size[0], $size[1], new ImagickPixel('#cccccc'));
 $im->setImageFormat('png');
 
 $tile_x = $tile_y = 0;
 
+// Рисуем карту
 for($x=$placement['min_x'];$x<=$placement['max_x'];$x++){
     $tile_y=0;
     for($y=$placement['max_y'];$y>=$placement['min_y'];$y--){
@@ -51,36 +51,12 @@ for($x=$placement['min_x'];$x<=$placement['max_x'];$x++){
 	   $wm->readImage($file);
         $im->compositeImage($wm, imagick::COMPOSITE_OVER,$placement['sh_x']+$tile_x*256,$placement['sh_y']-$tile_y*256);
         $tile_y++;
-        //break;
     }
-    //echo ($sh_x+$tile_x*256).'-';
     $tile_x++;
-    //break;
 }
-/*
 
-foreach($tiles as $tile){
-	// подгружаем все тайлы. Недостающие грузим из сети
-	if(!file_exists("../cache/".urlencode($tile['hash']))){
-		if(!check_host(base64_decode($tile['hash']))) break;
-		$img=file_get_contents(base64_decode($tile['hash']));
-		if(!$img or !file_put_contents("../cache/".urlencode($tile['hash']),$img)){
-			break;
-		}
-	}
-	$wm = new Imagick();
-	$wm->readImage("../cache/".urlencode($tile['hash']));
-	$matches=array();
-	preg_match_all('/(\-?\d+)/',$tile['displace'],$matches);
-	$displace=array($matches[1][4],$matches[1][5]);
-	$im->compositeImage($wm, imagick::COMPOSITE_OVER,$displace[0]-$x2,$displace[1]-$y2);
-}
-*/
-
-
-//exit; // <-- дебужим отсюда
 // Рисуем роут
-if(isset($path['path']) && $path['path'] && sizeof($path['path'])>1 && $path['color'] && $path['width']){
+if(false && isset($path['path']) && $path['path'] && sizeof($path['path'])>1 && $path['color'] && $path['width']){
     $dr = new ImagickDraw();
     $dr->setStrokeColor($path['color']);
     $dr->setFillColor('#ffffff00');
@@ -114,13 +90,11 @@ if(isset($path['path']) && $path['path'] && sizeof($path['path'])>1 && $path['co
             $im->compositeImage($wm, imagick::COMPOSITE_OVER,$x1+($x2-$x1)/2-13,$y1+($y2-$y1)/2-13);
             $wm->destroy();
         }
-        //echo 'length: '.$len.' @'.$ang.' ('.$x2.','.$x1.','.$y2.','.$y1.')<br>';
     }
 }
 
-if($points && sizeof($points) > 0){
+if(false && $points && sizeof($points) > 0){
     foreach($points as $point){
-        //echo '<pre>';print_r($point);exit;
         $dr = new ImagickDraw();
         $dr->setStrokeColor('#333333');
         $dr->setFillColor('#ffffff00');
@@ -154,10 +128,8 @@ if($points && sizeof($points) > 0){
         $dr->destroy();
     }
 }
-//exit;
 
-//$i=0;
-if($stickers && sizeof($stickers) > 0){
+if(false && $stickers && sizeof($stickers) > 0){
     foreach ($stickers as $sticker) {
         $svg = '<?xml version="1.0"?><svg width="120" height="120"><polygon  fill="#ff4433" points="60,60 70,22 98,22 98,50"></polygon></svg>';
         $wm = new Imagick();
@@ -208,79 +180,11 @@ if($stickers && sizeof($stickers) > 0){
         $wm->cropImage(72,72,$sticker['x'],0);
         $im->compositeImage($wm, imagick::COMPOSITE_OVER,$sticker['latlng']['x']-36+$x,$sticker['latlng']['y']-36+$y);
         $wm->destroy();
-        //$i++;
-        //if($i>2){ break; }
     }
 }
-//header('Content-type: image/png');
-//echo $im;
-//exit;
-/*
-	$polylines=array();
-	$cur_poly=0;
-	foreach($paths as $path){
-		if(!is_numeric($path['width']) || $path['width']<=0) $path['width']=3;
-		if(!$path['color'] || !preg_match('/^\#[a-f0-9]{6,8}$/',$path['color'])) $path['color']='#ff3333';
-		$polylines[$cur_poly]=array('color'=>$path['color'],'width'=>$path['width'],'path'=>array());
-		$matches=array();
-		preg_match_all('/\w[\-\d]+\s[\-\d]+/',$path['path'],$matches);
-		$i=0;$text='';
-		foreach($matches[0] as $var){
 
-			$op=substr($var,0,1);
-			$xy=explode(' ',substr($var,1));
+$raw = $im->getImageBlob();
 
-			if($op=='M'){
-				if(sizeof($polylines[$cur_poly]['path'])>0){
-					array_push($polylines[$cur_poly]['path'][sizeof($polylines[$cur_poly]['path'])-1],array('x'=>$xy[0]-$x1,'y'=>$xy[1]-$y1));
-				}
-
-				$polylines[$cur_poly]['path'][]=array(array('x'=>$xy[0]-$x1,'y'=>$xy[1]-$y1));
-			}elseif($op=='L'){
-				array_push($polylines[$cur_poly]['path'][sizeof($polylines[$cur_poly]['path'])-1],array('x'=>$xy[0]-$x1,'y'=>$xy[1]-$y1));
-			}
-
-			$i++;
-		}
-		$cur_poly++;
-	}
-	//echo '<pre>';
-	//print_r($polylines);exit;
-    $dr = new ImagickDraw();
-    foreach($polylines as $poly){
-    	$dr->setStrokeColor($poly['color']);
-    	$dr->setFillColor('#ffffff00');
-    	$dr->setStrokeWidth($poly['width']);
-    	foreach($poly['path'] as $var){
-			$dr->polyline($var);
-		}
-	}
-	$dr->setStrokeColor('#ffffff00');
-	$dr->setStrokeWidth(0);
-    $dr->setFont('RalewayBold.ttf');
-    $dr->setFontSize(14);
-    $dr->setFillColor('#ffffff');
-    //$dr->setStrokeAntialias(true);
-	//$dr->setTextAntialias(true);
-    //$dr->circle($polylines[0][0]['x'],$polylines[0][0]['y'],$polylines[0][0]['x']+5,$polylines[0][0]['y']+5);
-
-    //$text='Hello, Smitty!';
-    foreach($markers as $marker){
-	    $metrics = $im->queryFontMetrics($dr, $marker['text']);
-	    //echo '<pre>';print_r($metrics);exit;
-	    $dr->setFillColor($marker['color']);
-	    $dr->roundRectangle(
-	    	$marker['pos'][0]-$x1-8,
-	    	$marker['pos'][1]-$y1-$metrics['textHeight'],
-	    	$marker['pos'][0]-$x1+$metrics['textWidth']+16,
-	    	$marker['pos'][1]-$y1+12, 2, 2);
-	    $dr->setFillColor('#ffffff');
-	    $dr->annotation($marker['pos'][0]-$x1+4,$marker['pos'][1]-$y1+4, $marker['text']);
-	}
-    $im->drawImage($dr);
-*/
-
-$raw=$im->getImageBlob();
 if(isset($_REQUEST['mode']) && $_REQUEST['mode']=='test'){
 	header('Content-type: image/png');
 	echo $raw;
